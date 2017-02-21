77178a07b0aded70ad001f1c5e339802fe19f919
mariansurdu <surdu_marian94@yahoo.com>
2017-02-21 23:40:01 +0200

    Some tests

--- licenta/www/js/controllers.js
+++ licenta/www/js/controllers.js
@@ -53,7 +53,7 @@ angular.module('starter.controllers', [])
   })
 
 
-  .controller("DataCtrl",function($scope,$cordovaBluetoothLE){
+  .controller("DataCtrl",function($scope,$cordovaBluetoothLE,$ionicPlatform){
     $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
     $scope.series = ['Series A', 'Series B'];
 
@@ -61,5 +61,35 @@ angular.module('starter.controllers', [])
       [65, 59, 80, 81, 56, 55, 40],
       [28, 48, 40, 19, 86, 27, 90]
     ];
+
+    $ionicPlatform.ready(function() {
+      $cordovaBluetoothLE.initialize({request:true}).then(null,
+        function(obj) {
+          alert(JSON.stringify(obj));
+        },
+        function(obj) {
+          alert(JSON.stringify(obj));
+        }
+
+      );
+      $cordovaBluetoothLE.startScan({services:[]}).then(null,
+        function(obj) {
+          //Handle errors
+          console.log(obj.message);
+        },
+        function(obj) {
+          if (obj.status == "scanResult")
+          {
+            alert(JSON.stringify(obj));
+          }
+          else if (obj.status == "scanStarted")
+          {
+            alert("scan started")
+            alert(JSON.stringify(obj));
+          }
+        }
+
+    );
+    })
 
   })
