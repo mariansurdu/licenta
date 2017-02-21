217c84c9e3da97e2286cdb1d528be744343f5d4e
mariansurdu <surdu_marian94@yahoo.com>
2017-02-21 22:47:49 +0200

    added plugin bluetooth..nu merge nimic :(

--- licenta/www/js/controllers.js
+++ licenta/www/js/controllers.js
@@ -53,7 +53,7 @@ angular.module('starter.controllers', [])
   })
 
 
-  .controller("DataCtrl",function($scope){
+  .controller("DataCtrl",function($scope,$cordovaBluetoothLE){
     $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
     $scope.series = ['Series A', 'Series B'];
 
